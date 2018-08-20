c2b88ea5243e8b14e1fdf5c1f28a3a9997437546
Jack <jack@digilabs.io>
2018-08-20 10:49:15 -0500

    Added republishing of images

--- src/image_sub_node.cpp
+++ src/image_sub_node.cpp
@@ -19,6 +19,7 @@ class ImageSubNode {
 
     // if set, we publish the images that contain fiducials
     bool publish_images;
+    image_transport::Publisher image_pub;
 
     bool haveCamInfo;
     int frameNum; 
@@ -37,7 +38,9 @@ void ImageSubNode::camInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& msg)
 }
 
 void ImageSubNode::imageCallback(const sensor_msgs::ImageConstPtr & msg) {
-    ROS_INFO("Got image %d", msg->header.seq);
+    ROS_INFO("Got image %d. Republishing", msg->header.seq);
+    //sensor_msgs::ImagePtr msg2 = msg;
+    image_pub.publish(msg);
 }
 
 ImageSubNode::ImageSubNode(ros::NodeHandle & nh) : it(nh)
@@ -45,6 +48,7 @@ ImageSubNode::ImageSubNode(ros::NodeHandle & nh) : it(nh)
     img_sub = it.subscribe("/teleop_cam", 1, &ImageSubNode::imageCallback, this);
 
     caminfo_sub = nh.subscribe("/camera_info", 1, &ImageSubNode::camInfoCallback, this);
+    image_pub = it.advertise("/repub_images", 1);
 
     ROS_INFO("Subscribed to image and camera_info");
 }
